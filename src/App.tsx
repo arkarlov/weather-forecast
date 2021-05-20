c8b3ff3dc4659249ec73ce486287b98f69e2c4c9
ArtemKarlov <ar.karlov@gmail.com>
2021-05-20 10:32:46 +0400

    feat: create main component

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,49 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+import ForecastCard from './components/forecast-card/forecast-card'
+import Placeholder from './components/placeholder/placeholder'
+import WeatherCard from './components/weather-card/weather-card'
+import placeholderImage from './assets/Placeholder/Academy-Weather-bg160.png'
+import { CitySelector, DateSelector } from './components/selectors/selectors'
+import GetData from './controllers/get-data'
+import Carousel from './components/carousel/carousel'
+
+const cities = ["moscow", "samara", "kazan", "saratov", "volgograd", "izhevsk", 'penza']
+
 function App () {
+  const date = new Date();
+  const icon = placeholderImage
+  const caption = 'test'
+  const temperature = '+17'
+
+  GetData()
+
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1 className="app__title">
+        <span className="app__title-top">weather</span>
+        <span className="app__title-bottom">forecast</span>
+      </h1>
+
+      <main className="app__main">
+        <ForecastCard 
+          cardTitle = { '7 Days Forecast' }
+          citySelector = { <CitySelector cities = {cities} /> }
+          content ={ <Carousel /> }
+        />
+        <ForecastCard 
+          cardTitle = { 'Forecast for a Date in the Past' }
+          citySelector = { <CitySelector cities = {cities} /> }
+          dateSelector = {<DateSelector /> }
+          content ={ <WeatherCard date={date.toLocaleDateString('ru-RU', {year: 'numeric', month: 'short', day: 'numeric'})} icon={icon} caption={caption} temperature={temperature} /> }
+        />
+      </main>
+
+      <footer className="app__footer">
+        <p className="app__footer-caption">C ЛЮБОВЬЮ ОТ MERCURY DEVELOPMENT</p>
+      </footer>
     </div>
   )
 }
