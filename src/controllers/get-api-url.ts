caa0c553fe7f1a7b05088df16909a103f2729721
ArtemKarlov <ar.karlov@gmail.com>
2021-05-21 13:42:11 +0400

    faet: add url creator

---
+++ src/controllers/get-api-url.ts
@@ -0,0 +1,20 @@
+const API_BASE_URL = 'https://api.openweathermap.org'
+const API_KEY = '4e7170fdaf57f6c481ced0713409d77d'
+
+
+export default function getUrl (lat: number, lon: number, isPast = false, dt?: string): string {
+  
+  const urlString = `/data/2.5/onecall${isPast ? `/timemachine`: ``}`
+  const url = new URL(urlString, API_BASE_URL);
+
+  url.searchParams.append('lat', lat.toString());
+  url.searchParams.append('lon', lon.toString());
+  if (isPast) {
+    if (dt) url.searchParams.append('dt', dt)
+  } else {
+    url.searchParams.append('exclude', 'minutely,hourly,alerts')
+  }
+  url.searchParams.append('appid', API_KEY)
+
+  return url.toString()
+}
