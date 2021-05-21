e7a47541fa8ed582fca3556774552047801bf440
ArtemKarlov <ar.karlov@gmail.com>
2021-05-22 00:57:32 +0400

    refactor: problem: load mixed content - use https for icons

--- src/controllers/api-response-handler.ts
+++ src/controllers/api-response-handler.ts
@@ -7,7 +7,7 @@ export function handleDailyResponse (dailyData: ForecastData) {
   return daily.map((day) => {
     const date = new Date(day.dt * 1000).toLocaleDateString('ru-RU', {year: 'numeric', month: 'short', day: 'numeric'})
     const temperature = Math.round(day.temp.day - 273.15).toString()
-    const icon = `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`
+    const icon = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`
     const caption = day.weather[0].description
 
     return {date, temperature, icon, caption}
@@ -19,7 +19,7 @@ export function handlePastResponse (PastData: ForecastData) {
 
   const date = new Date(current.dt * 1000).toLocaleDateString('ru-RU', {year: 'numeric', month: 'short', day: 'numeric'})
   const temperature = Math.round(current.temp - 273.15).toString()
-  const icon = `http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`
+  const icon = `https://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`
   const caption = current.weather[0].description
 
   return {date, temperature, icon, caption}
