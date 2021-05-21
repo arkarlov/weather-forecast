6c495a7b18c7c24449ff9b2e7700cfb75193517e
ArtemKarlov <ar.karlov@gmail.com>
2021-05-21 18:19:34 +0400

    refactor: get props from selector into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,82 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useState } from 'react'
 import './App.css'
+import { CITIES } from './global'
 
 import ForecastCard from './components/forecast-card/forecast-card'
 import Placeholder from './components/placeholder/placeholder'
-import WeatherCard from './components/weather-card/weather-card'
+import WeatherCard, { WeatherCardData } from './components/weather-card/weather-card'
 import placeholderImage from './assets/Placeholder/Academy-Weather-bg160.png'
 import { CitySelector, DateSelector } from './components/selectors/selectors'
 import getData from './controllers/get-api-data'
 import Carousel from './components/carousel/carousel'
 import getUrl from './controllers/get-api-url'
 import getCityCoords from './controllers/get-city-coords'
+import handleDailyResponse from './controllers/api-response-handler'
 
-const cities = ["moscow", "samara", "kazan", "saratov", "volgograd", "izhevsk", 'penza']
 
 function App () {
+
+    
+  const cities = CITIES.map((elem) => elem.name)
+
+  const [cityDaily, setCityDaily] = useState('default')
+  const [cityPast, setCityPast] = useState('default')
+  const [datePast, setDatePast] = useState('')
+  const [isDataLoaded, setIsDataLoaded] = useState(false)
+
   const date = new Date();
   const icon = placeholderImage
   const caption = 'test'
   const temperature = '+17'
 
-  async function showData () {
-    const {lat, lon} = getCityCoords('samara')
-    console.log(await getData(getUrl(lat, lon)))
+
+  
+  let carouselData: Array<WeatherCardData> =  [{
+    date: 'string',
+    icon: 'string',
+    caption: 'string',
+    temperature: 'string'
+  },
+  {
+    date: 'strin546g',
+    icon: 'string',
+    caption: 'string',
+    temperature: 'string'
+  },{
+    date: 'str345ing',
+    icon: 'string',
+    caption: 'string',
+    temperature: 'string'
+  },{
+    date: 'str23ing',
+    icon: 'string',
+    caption: 'string',
+    temperature: 'string'
+  }]
+
+  async function showData (cityName: string) {
+    const {lat, lon} = getCityCoords(cityName)
+    const forecastData = await getData(getUrl(lat, lon))  
+    carouselData = handleDailyResponse(forecastData) 
+    setIsDataLoaded(true)
+    console.log(carouselData)
+  }  
+
+
+
+
+  const getCity = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    if (event.target.id == 'city-selector-1') {
+      setCityDaily(event.target.value)
+      showData(event.target.value)
+    } else {
+      setCityPast(event.target.value)
+    }
   }
 
-  showData()
+  const getDate = (inputDateValue: string) => {
+    setDatePast(inputDateValue)
+  }
 
   return (
     <div className='app'>
@@ -37,14 +88,14 @@ function App () {
       <main className="app__main">
         <ForecastCard 
           cardTitle = { '7 Days Forecast' }
-          citySelector = { <CitySelector cities = {cities} /> }
-          content ={ <Carousel /> }
+          citySelector = { <CitySelector id='city-selector-1' cities = {cities} handleSelect = { getCity } /> }
+          content ={ cityDaily == 'default' || !isDataLoaded  ? <Placeholder /> : <Carousel weatherData = {carouselData} /> }
         />
         <ForecastCard 
           cardTitle = { 'Forecast for a Date in the Past' }
-          citySelector = { <CitySelector cities = {cities} /> }
-          dateSelector = {<DateSelector /> }
-          content ={ <WeatherCard date={date.toLocaleDateString('ru-RU', {year: 'numeric', month: 'short', day: 'numeric'})} icon={icon} caption={caption} temperature={temperature} /> }
+          citySelector = { <CitySelector id='city-selector-2' cities = {cities} handleSelect = { getCity }/> }
+          dateSelector = {<DateSelector id='date-selector-1' handleSelect = {getDate} /> }
+          content ={ cityPast == 'default' || datePast == '' ? <Placeholder /> : <WeatherCard date={new Date(datePast).toLocaleDateString('ru-RU', {year: 'numeric', month: 'short', day: 'numeric'})} icon={icon} caption={caption} temperature={temperature} /> }
         />
       </main>
 
