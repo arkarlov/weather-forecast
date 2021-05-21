ecee93513d055c68bf6edf120fcf063bc582c2f5
ArtemKarlov <ar.karlov@gmail.com>
2021-05-21 23:48:19 +0400

    feat: add sliding carousel

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -12,7 +12,18 @@ function Carousel (props: CarouselProps) {
   const { weatherData } = props
 
 
-  const showCardFrom = 1
+  const [firstShownCardNumber, setFirstShownCardNumber] = useState(1)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    let index = firstShownCardNumber
+    if (event.currentTarget.id == 'button-next') {
+      index++
+      setFirstShownCardNumber(index)
+    } else {
+      index--
+      setFirstShownCardNumber(index)
+    }
+  }
 
   const weatherCards = weatherData.map((data) => {
     return <WeatherCard key={data.date} date={ data.date } icon={ data.icon } caption={ data.caption } temperature={ data.temperature } />
@@ -20,13 +31,13 @@ function Carousel (props: CarouselProps) {
 
   return (
     <div className="carousel" >
-      <button className="carousel__button carousel__button_left button button_prev button_disabled"></button>
+      <button id="button-prev" className={`carousel__button carousel__button_left button button_prev ${firstShownCardNumber == 1 ? `button_disabled` : ``}`} onClick={ handleClick }></button>
       <div className="carousel__content">
         <div className="carousel__track">
           {
                         
             weatherCards.map((card, index) => {
-              if (index >= showCardFrom && index < showCardFrom + 3) {
+              if (index >= firstShownCardNumber && index < firstShownCardNumber + 3) {
                 return card
               }
             })
@@ -34,7 +45,7 @@ function Carousel (props: CarouselProps) {
           }
         </div>
       </div>            
-      <button className="carousel__button carousel__button_right button button_next"></button>
+      <button id="button-next" className={`carousel__button carousel__button_right button button_next ${firstShownCardNumber == 5 ? `button_disabled` : ``}`} onClick={ handleClick }></button>
     </div>
   )
 
