40f86a3aa8fec0ee9b058be18c785c27b460893f
ArtemKarlov <ar.karlov@gmail.com>
2021-05-21 23:05:49 +0400

    refactor: remove not needed param in get url function

--- src/controllers/get-api-url.ts
+++ src/controllers/get-api-url.ts
@@ -2,15 +2,15 @@ const API_BASE_URL = 'https://api.openweathermap.org'
 const API_KEY = '4e7170fdaf57f6c481ced0713409d77d'
 
 
-export default function getUrl (lat: number, lon: number, isPast = false, dt?: string): string {
+export default function getUrl (lat: number, lon: number, dt?: string): string {
   
-  const urlString = `/data/2.5/onecall${isPast ? `/timemachine`: ``}`
+  const urlString = `/data/2.5/onecall${dt ? `/timemachine`: ``}`
   const url = new URL(urlString, API_BASE_URL);
 
   url.searchParams.append('lat', lat.toString());
   url.searchParams.append('lon', lon.toString());
-  if (isPast) {
-    if (dt) url.searchParams.append('dt', dt)
+  if (dt) {
+    url.searchParams.append('dt', dt)
   } else {
     url.searchParams.append('exclude', 'minutely,hourly,alerts')
   }
